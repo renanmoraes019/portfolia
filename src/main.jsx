426fb901e0f29977e14ce1bce1b2a7efd3c1051a
renanmoraes019 <renan.moraes.16almeida@gmail.com>
2024-11-04 19:56:01 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import Header from './components/Header'
 import AboutSection from './components/AboutSection'
 import PortfolioSection from './components/PortfolioSectiom'
 import ContactSection from './components/ContactSection'
+import HeroSection from './components/HeroSection'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
+    <HeroSection />
     <AboutSection />
     <PortfolioSection />
     <ContactSection />
