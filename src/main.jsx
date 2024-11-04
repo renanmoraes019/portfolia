b0bafde6f262cb090232b53c723d85f2630b88a5
renanmoraes019 <renan.moraes.16almeida@gmail.com>
2024-11-04 19:53:58 -0300

    testando build

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './components/Header'
-import HeroSection from './components/HeroSection'
 import AboutSection from './components/AboutSection'
 import PortfolioSection from './components/PortfolioSectiom'
 import ContactSection from './components/ContactSection'
@@ -10,7 +9,6 @@ import ContactSection from './components/ContactSection'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-    <HeroSection />
     <AboutSection />
     <PortfolioSection />
     <ContactSection />
